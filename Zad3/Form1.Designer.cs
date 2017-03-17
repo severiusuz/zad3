4d7a3ab50ac2a272f5bf992086703573ab100565
severiusuz <severiusuz@gmail.com>
2017-03-17 13:39:00 +0100

    Supa

---
+++ Zad3/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Zad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(41, 35);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(191, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Click me";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 102);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Zad3/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zad3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Zupelnie inny tekst.",
+                "Nieważna wiadomość");
+            
+            DialogResult result2 = MessageBox.Show("Czy wierzysz że to nieważna wiadomość?",
+                "Niewazna wiadomość",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            
+            DialogResult result3 = MessageBox.Show("Czy to jest pytanie?",
+                "To nie pytanie",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button3);
+            
+            MessageBox.Show("Nie zdałeś",
+                "To nie krytyczna wiadomość",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.RightAlign,
+                false);
+            
+            MessageBox.Show("Potwierdź, że nie zdałeś",
+                "Nie ważne co klikniesz",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+        }
+    }
+}
